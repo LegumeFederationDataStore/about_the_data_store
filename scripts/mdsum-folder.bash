47af20a2c480d292edb86cec2f423b4c4137c608
cann0010 <scannon@iastate.edu>
2020-04-09 15:41:39 -0500

    add mdsum-folder.bash

--- scripts/mdsum-folder.bash
+++ scripts/mdsum-folder.bash
@@ -12,5 +12,5 @@ md5_name=CHECKSUM.$key.md5
 
 if [[ -e $md5_name ]]; then rm $md5_name; fi
 #FIXME: this will work on LIS using BSD. if anyone else ever uses it, will need to test OS and adjust accordingly I think
-md5 -r * > $md5_name
+find . -type f | xargs md5 -r > $md5_name
 popd
